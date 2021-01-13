d9d5f48406e93351cd91848f559a71258f114fb4
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-13 13:00:29 -0800

    html setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
 
+const throwButton = document.getElementById('throw-button');
+const userWins = document.getElementById('wins');
+const throwButton = document.getElementById('throw-button');
+const throwButton = document.getElementById('throw-button');
+
+
+
+
 // initialize state
 
 // set event listeners to update state and DOM
