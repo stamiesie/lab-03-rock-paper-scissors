5d788acc85571ed2b872cfa890c831c2079ec73d
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-13 13:23:46 -0800

    DOM elements, add event listener to button

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 
 const throwButton = document.getElementById('throw-button');
 const userWins = document.getElementById('wins');
-const throwButton = document.getElementById('throw-button');
-const throwButton = document.getElementById('throw-button');
-
-
-
+const userLoses = document.getElementById('losses');
+const userDraws = document.getElementById('draws');
+const totalPlays = document.getElementById('total');
+const gameResult = document.getElementById('game-result');
 
 // initialize state
 
+let wins = 0;
+let losses = 0;
+let draws = 0;
+let total = 0;
+
 // set event listeners to update state and DOM
+
+throwButton.addEventListener('click', () => {
+
+    console.log('click!');
+
+
+
+
+})
